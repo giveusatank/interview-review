cc3708919d7e9f190b17152b50b6609a4aa74953
QINZ <1181047207@qq.com>
2020-03-02 22:10:19 +0800

    third submit interview review

--- src/main/java/com/interview/git/GitSyllabus.java
+++ src/main/java/com/interview/git/GitSyllabus.java
@@ -8,5 +8,7 @@ public class GitSyllabus {
         System.out.println("4444444444");
         System.out.println("5555555555");
         System.out.println("6666666666");
+        System.out.println("7777777777");
+        System.out.println("8888888888");
     }
 }
